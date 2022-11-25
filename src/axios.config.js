066ed715563a47acdb5d4807b86ef3c1986d237a
Eyoel Tewodros <eyoeltewodros93@gmail.com>
2022-11-25 16:33:46 +0300

    cors issue on mobile fixed

--- src/axios.config.js
+++ src/axios.config.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     environment === "dev"
       ? "http://localhost:5000/api/"
-      : "https://dev-exam-api.onrender.com/api/",
+      : "https://dev-exam-api-production.up.railway.app/api/",
 });
 
 export default axiosInstance;
